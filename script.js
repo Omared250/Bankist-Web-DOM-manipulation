b268cc6b46e82d5572fbf76ff9d39c02ec9efd74
Omar <omared250@gmail.com>
2022-03-13 13:47:27 +0100

    new way to add a scroll function to our project

--- script.js
+++ script.js
@@ -39,9 +39,10 @@ btnScrollTo.addEventListener('click', function(e) {
   const s1coords = section1.getBoundingClientRect();
   
   // Scrolling
-  window.scrollTo({
-    left : s1coords.left + window.pageXOffset,
-    top : s1coords.top + window.pageYOffset,
-    behavior : 'smooth',
-  });
+  // window.scrollTo({
+  //   left : s1coords.left + window.pageXOffset,
+  //   top : s1coords.top + window.pageYOffset,
+  //   behavior : 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth'});
 })

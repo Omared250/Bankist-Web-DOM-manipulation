4b1354acda431bdbede070c0b5598ff331417ced
Omar <omared250@gmail.com>
2022-03-20 14:25:03 +0100

    selecting elements to work in the tapped component

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const btnLinks = document.querySelectorAll('.nav__link');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
@@ -51,3 +54,5 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
       .scrollIntoView({ behavior : 'smooth'});
   }
 })
+
+// Tabbed component

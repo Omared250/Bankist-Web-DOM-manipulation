5ca5bab368cdffa6f0a1df0dee366fb9503fdba2
Omar <omared250@gmail.com>
2022-03-21 21:02:41 +0100

    adding opacity feature to the nav bar

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const btnLinks = document.querySelectorAll('.nav__link');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
 // Modal window
@@ -72,4 +73,25 @@ tabsContainer.addEventListener('click', function(e){
   // Active content area
   document.querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active')
+})
+
+// Menu fade animation
+nav.addEventListener('mouseover', function(e){
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = 0.5
+    });
+    logo.style.opacity = 0.5;
+  }
+})
+
+nav.addEventListener('mouseout', function(e){
+  siblings.forEach(el => {
+    el.style.opacity = 1;
+  });
+  logo.style.opacity = 1;
 })
